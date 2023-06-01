754e1da38b8dc436b65b54e4fa4987af64de2b1d
Andrii Sadovyi <sadoviya@gmail.com>
2023-06-01 16:52:23 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,21 @@
 import SimpleLightbox from 'simplelightbox';
-// Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const containerRef = document.querySelector('.gallery');
+containerRef.innerHTML = createGalleryItem(galleryItems);
+
+function createGalleryItem(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}"
+      alt="${description}"
+      title="${description}">
+      </a>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
