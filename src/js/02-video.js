640c2bb7dfa597038d492323602e71180d8ef19a
Andrii Sadovyi <sadoviya@gmail.com>
2023-06-04 21:36:30 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayerRef = document.querySelector('#vimeo-player');
+const player = new Player(vimeoPlayerRef);
+
+player.on(
+  'timeupdate',
+  throttle(value => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(value));
+  }, 1000)
+);
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+if (currentTime !== null) player.setCurrentTime(currentTime.seconds);
